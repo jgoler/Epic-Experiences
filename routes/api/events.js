ae94bf5544b893f0eaab1c99416403481fd8f77f
Jack Goler <jack.goler@gmail.com>
2020-11-10 10:27:09 -0800

    Finished routes

--- routes/api/events.js
+++ routes/api/events.js
@@ -5,7 +5,7 @@ const Event = require('../../models/Event');
 
 
 // @route  POST api/events
-// @desc   Register user
+// @desc   Register event
 // @access Public
 router.post(
   '/',
@@ -40,5 +40,61 @@ router.post(
       res.status(500).send('Server error');
     }
   });
+
+// @route  GET api/events
+// @desc   Gets events
+// @access Public
+router.get('/', async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  GET api/events/event:event_id
+// @desc   Get specific event by ID
+// @access Public
+router.get('/event/:event_id', async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.event_id);
+    //console.log(event);
+
+    if (!event) return res.status(400).json({ msg: 'There is no matching event' });
+
+    res.json(event);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  GET api/events/:category
+// @desc   Get events in a specific category
+// @access Public
+router.get('/:category', async (req, res) => {
+  try {
+    const events = await Event.find({ category: req.params.category });
+    res.json(events);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Sever Error');
+  }
+});
+
+/*
+router.get('/:date', async (req, res) => {
+  try {
+    const events = await Event.find({ date: req.params.date });
+    res.json(events);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+*/
+
 
 module.exports = router;
